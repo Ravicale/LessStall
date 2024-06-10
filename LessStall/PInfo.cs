ee35faa5f74725085c9f186e44b3f4a5680db684
Ravicale <epicnetmail@gmail.com>
2024-06-09 23:24:16 -0500

    Increment version.

--- LessStall/PInfo.cs
+++ LessStall/PInfo.cs
@@ -5,7 +5,7 @@ namespace LessStall {
         // each loaded plugin needs to have a unique GUID. usually author+generalCategory+Name is good enough
         public const string GUID = "ravi.lbol.lessStall";
         public const string Name = "Less Stall";
-        public const string version = "1.0.0";
+        public const string version = "1.0.1";
         public static readonly Harmony harmony = new Harmony(GUID);
 
     }
